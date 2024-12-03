5f3b8309ded5b32519182088a2daa35d16b9fbc4
omarelbeshbichi <94106234+omarelbeshbichi@users.noreply.github.com>
2024-12-03 20:03:46 +0100

    Solve JS 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32)*(5/9);
+
+  if (tempC !== 0) {
+    tempC = tempC.toFixed(1);
+  }
+  else {
+    tempC = tempC.toFixed(0);
+  };
+
+  return Number(tempC);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC*9/5) + 32;
+
+  if (tempF !== 0) {
+    tempF= tempF.toFixed(1);
+  }
+  else {
+    tempF = tempF.toFixed(0);
+  };
+
+  return Number(tempF);
 };
 
 // Do not edit below this line
