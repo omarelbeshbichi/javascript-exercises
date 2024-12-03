1ade88399e4f55a2c931c843888906f2304f33d2
omarelbeshbichi <94106234+omarelbeshbichi@users.noreply.github.com>
2024-12-03 19:41:32 +0100

    Solve 5 JS

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    ans = 0;
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+        else if (!Number.isInteger(num1) || !Number.isInteger(num2) ) {
+        return "ERROR";
+    }
+    else if (typeof num1 === String || typeof num2 === String ) {
+        return "ERROR";
+    }
+    else if (typeof num1 !== "number"  || typeof num2 !== "number" ) {
+        return "ERROR";
+    };
+
+    for (let i = Math.min(num1, num2); i <= Math.max(num1,num2); i++) {
+
+        ans += i;
+
+    };
+
+    return ans;
 
 };
 
