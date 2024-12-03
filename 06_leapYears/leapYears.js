3947065c7d0b003d3c835c57e7b8b97de4fd589d
omarelbeshbichi <94106234+omarelbeshbichi@users.noreply.github.com>
2024-12-03 19:54:43 +0100

    Solve JS 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    ans = true;
+
+    if (year % 4 === 0 ) {
+        ans = true; 
+        if (year % 100 === 0) {
+            ans = false;
+            if (year % 400 === 0) {
+                ans = true;
+            };
+        };       
+    }
+    else {
+        ans = false;
+    };
+
+    return ans;
 
 };
 
