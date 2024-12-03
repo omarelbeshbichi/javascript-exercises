6e1d4b41c2465af1530e819ae23f7408fc82969c
omarelbeshbichi <94106234+omarelbeshbichi@users.noreply.github.com>
2024-12-03 19:20:25 +0100

    Solve JS 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    
+    for (let i = 0; i<values.length; i++) {
+        
+        array = array.filter(item => item !== values[i]);
+    };
+
+    return array;
+    
 };
 
 // Do not edit below this line
