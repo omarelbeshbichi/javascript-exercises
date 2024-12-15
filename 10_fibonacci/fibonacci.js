9249794e502aa1d2d4637347d660dd688cc0e4dc
omarelbeshbichi <94106234+omarelbeshbichi@users.noreply.github.com>
2024-12-15 19:20:56 +0100

    Solve exercise 10 JS

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+   
+
+
+if (num >= 0) {
+    if (num >= 3) {
+        let fibonacci = [1, 1];
+    for(let i=2; i <=num; i++) {
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];   
+    }
+    return fibonacci[num-1]
+    } 
+        else if (num == 0) {
+            return 0
+        }
+        else {    
+            return 1
+        }
+}
+else {
+    return "OOPS"
+}
+    
+
 
 };
 
