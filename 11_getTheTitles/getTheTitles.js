5042b00fe6ef9576a1218a7ea23495caa80e8b83
omarelbeshbichi <94106234+omarelbeshbichi@users.noreply.github.com>
2024-12-15 19:27:42 +0100

    Solve exercise 11 JS

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+
+    return arr.reduce((finalArray, obj) => {
+        finalArray.push(obj.title);
+        return finalArray
+    }, [])
 
 };
 
