8b084681b41f49f2cdf6d3280684597de90e5981
omarelbeshbichi <94106234+omarelbeshbichi@users.noreply.github.com>
2024-12-15 19:00:51 +0100

    Solve exercise 09 JS

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    const stringClean = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+    let  palindromeString = stringClean.split("").reverse().join("");
+    if (palindromeString === stringClean) {
+        return true
+    }
+    else {
+        return false
+    }
 
 };
 
