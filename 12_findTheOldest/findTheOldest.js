3c32632b2dead3ea2dd0ad9d8946502f0c49dded
omarelbeshbichi <94106234+omarelbeshbichi@users.noreply.github.com>
2024-12-15 20:03:02 +0100

    Solve exercise 12 JS

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+    const currentYear = new Date().getFullYear();
+
+    //arrFiltered = arr.filter((obj) => obj.hasOwnProperty("yearOfDeath"));
+    
+    let arrSorted = arr.sort((objA, objB) => {
+        
+        const bDeath = objB.yearOfDeath || currentYear;
+        const aDeath = objA.yearOfDeath || currentYear;
+
+        const bAge = bDeath - objB.yearOfBirth;
+        const aAge = aDeath - objA.yearOfBirth;
+        
+        return bAge - aAge
+    });
+    
+    return arrSorted[0]
 
 };
 
